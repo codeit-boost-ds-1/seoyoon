b006e4b58bffa58983a2f74637c41ee23053a5b0
pookey1104 <pookey1104@naver.com>
2024-11-08 22:51:26 +0900

    [17주차] QRCode

---
+++ week17/QRCode/shortit/.next/server/font-loader-manifest.js
@@ -0,0 +1,8 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/a15f2fce4b98b461.p.woff2"
+    ]
+  },
+  "app": {}
+}
---
+++ week17/QRCode/shortit/pages/qrcodes/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import QRCodeList from '@/components/QRCodeList';
+import Button from '@/components/Button';
+import Link from '@/components/Link';
+import styles from '@/styles/QRCodeListPage.module.css';
+import dbConnect from '@/db/dbConnect';
+import QRCode from '@/db/models/QRCode';
+
+export async function getServerSideProps() {
+  await dbConnect();
+  const qrCodes = await QRCode.find();
+  return {
+    props: {
+      qrCodes: JSON.parse(JSON.stringify(qrCodes)),
+    },
+  };
+}
+
+export default function QRCodeListPage({ qrCodes }) {
+  return (
+    <>
+      <Head>
+        <title>QRCode 만들기 - Shortit</title>
+      </Head>
+      <div className={styles.page}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>QRCode 만들기</h1>
+          <Button as={Link} href="/qrcodes/new">
+            새로 만들기
+          </Button>
+        </header>
+        <QRCodeList items={qrCodes}/>
+      </div>
+    </>
+  );
+}
